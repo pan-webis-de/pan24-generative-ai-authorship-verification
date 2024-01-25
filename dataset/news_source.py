ba45b426c1f92412e883854ec46ddb2f36b89ee1
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2024-01-25 11:44:31 +0100

    Download articles

--- dataset/news_source.py
+++ dataset/news_source.py
@@ -1,9 +1,16 @@
+import glob
 import json
+import logging
 import os
 import time
 
 import click
 import gnews
+import newspaper
+import requests
+
+
+logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -24,7 +31,7 @@ def list_news(start_date, end_date, topic_file, output, language, country, num_r
     output = os.path.join(output, 'lists')
     os.makedirs(output, exist_ok=True)
 
-    with click.progressbar(topic_file, label='Downloading news for topics') as progress:
+    with click.progressbar(topic_file.readlines(), label='Downloading news for topics') as progress:
         for topic in progress:
             topic = topic.strip()
             if not topic:
@@ -43,6 +50,56 @@ def list_news(start_date, end_date, topic_file, output, language, country, num_r
                     f.write('\n')
 
             time.sleep(sleep_time)
+
+
+@main.command(help='Download posts from news lists')
+@click.argument('input_dir', type=click.Path(file_okay=False, exists=True))
+@click.option('-o', '--output', type=click.Path(file_okay=False), help='Output directory', default='data')
+def scrape_posts(input_dir, output):
+    output = os.path.join(output, 'posts')
+    os.makedirs(output, exist_ok=True)
+
+    newspaper_cfg = newspaper.Config()
+    newspaper_cfg_browser = newspaper.Config()
+    newspaper_cfg_browser.headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+
+    with click.progressbar(glob.glob(os.path.join(input_dir, '*.jsonl')), label='Downloading news posts') as progress:
+        for news_list in progress:
+            d = os.path.join(output, os.path.basename(news_list[:-6]))
+            os.makedirs(d, exist_ok=True)
+
+            for i, news_item in enumerate(open(news_list, 'r')):
+                art_id = f'art-{i:03d}'
+                out_name_html = os.path.join(d, f'{art_id}.html')
+                out_name_txt = os.path.join(d, f'{art_id}.txt')
+                if os.path.exists(out_name_html) and os.path.exists(out_name_txt):
+                    continue
+
+                news_item = json.loads(news_item)
+
+                # First, retrieve target URL with a non-browser User-Agent.
+                article_url = requests.head(news_item['url']).headers.get('Location')
+
+                # Second, scrape the article itself with a browser User-Agent.
+                # We cannot do both in one go, since Google displays cookie banners for browsers, but
+                # some news pages block scrapers / non-browsers.
+                try:
+                    cfg = newspaper_cfg_browser if 'reuters.com' in article_url else newspaper_cfg
+                    article = newspaper.Article(url=article_url or news_item['url'], config=cfg, language='en')
+                    article.download()
+                    article.parse()
+                except newspaper.article.ArticleException:
+                    logger.error('Failed to download %s/%s (URL: %s)', d,  art_id, article_url)
+                    continue
+
+                text = '\n\n'.join((article.title, article.text))
+                open(out_name_html, 'w').write(article.html)
+                open(out_name_txt, 'w').write(text)
 
 
 if __name__ == "__main__":
