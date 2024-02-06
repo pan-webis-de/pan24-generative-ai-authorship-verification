7ea654e81b4a4d417842ccf4fe8b5c710cf05045
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2024-02-06 16:10:06 +0100

    Add script to build and push Docker image

---
+++ build-docker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t registry.webis.de/code-research/authorship/pan24-generative-authorship-detection .
+docker push registry.webis.de/code-research/authorship/pan24-generative-authorship-detection
