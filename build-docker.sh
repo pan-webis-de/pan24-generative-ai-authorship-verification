c58c25e25d8cb5281c4c9b8bc0a146e7ee84c61d
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2024-02-23 14:33:54 +0100

    Allow optional parameters for docker build

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-docker build -t registry.webis.de/code-research/authorship/pan24-generative-authorship-detection .
+docker build -t registry.webis.de/code-research/authorship/pan24-generative-authorship-detection "$@" .
 docker push registry.webis.de/code-research/authorship/pan24-generative-authorship-detection
